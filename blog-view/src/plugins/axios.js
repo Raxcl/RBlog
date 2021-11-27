28b08a1e69e81cdb16b05ab53972012ca4bb4d49
raxcl <70327089+raxcl@users.noreply.github.com>
2021-11-27 17:28:05 +0800

    部署上线

--- blog-view/src/plugins/axios.js
+++ blog-view/src/plugins/axios.js
@@ -3,7 +3,8 @@ import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
 const request = axios.create({
-	baseURL: 'http://localhost:8090/',
+	// baseURL: 'http://localhost:8090/',
+	baseURL: process.env.VUE_APP_URL,
 	timeout: 10000,
 })
 
--- blog-view/vue.config.js
+++ blog-view/vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+	publicPath:'./', //从当前目录下获取资源
+	outputDir: process.env.outputDir,    // 打包生成目录
 	configureWebpack: {
 		resolve: {
 			alias: {
