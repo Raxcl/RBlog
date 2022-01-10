be9dde70515af324c118ea241ba2c9985740a3a4
raxcl <70327089+raxcl@users.noreply.github.com>
2022-01-10 22:26:53 +0800

    Update SpringContextUtils.java

--- blog-api/src/main/java/cn/raxcl/util/common/SpringContextUtils.java
+++ blog-api/src/main/java/cn/raxcl/util/common/SpringContextUtils.java
@@ -11,39 +11,25 @@ import org.springframework.stereotype.Component;
  * @date 2022-01-07 18:58:02
  */
 @Component
-//TODO
 public class SpringContextUtils implements ApplicationContextAware {
+
 	private static ApplicationContext applicationContext;
 
 	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException{
+		SpringContextUtils.setContent(applicationContext);
+	}
+
+	private static void setContent(ApplicationContext applicationContext) {
 		SpringContextUtils.applicationContext = applicationContext;
 	}
 
+	/**
+	 * 通过bean名称获取上下文中的bean
+	 * @param name bean名称
+	 * @return Object
+	 */
 	public static Object getBean(String name) {
 		return applicationContext.getBean(name);
-	}
-
-	public static <T> T getBean(String name, Class<T> requiredType) {
-		return applicationContext.getBean(name, requiredType);
-	}
-
-	public static boolean containsBean(String name) {
-		return applicationContext.containsBean(name);
-	}
-
-	public static boolean isSingleton(String name) {
-		return applicationContext.isSingleton(name);
-	}
-
-	public static Class<? extends Object> getType(String name) {
-		return applicationContext.getType(name);
-	}
-
-	/**
-	 * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
-	 */
-	public static <T> T getBean(Class<T> requiredType) {
-		return applicationContext.getBean(requiredType);
 	}
 }
