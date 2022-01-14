46b033afad56e07e452733744a80cf8ce16fd3e5
raxcl <70327089+raxcl@users.noreply.github.com>
2022-01-14 22:14:25 +0800

    修正页面刷新白屏问题

--- blog-view/vue.config.js
+++ blog-view/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	publicPath:'./', //从当前目录下获取资源
+	// publicPath:'./', //从当前目录下获取资源（由于静态资源不太一起，加了刷新页面会白屏）
 	outputDir: process.env.outputDir,    // 打包生成目录
 	configureWebpack: {
 		resolve: {
