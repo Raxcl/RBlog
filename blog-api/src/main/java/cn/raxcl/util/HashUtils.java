d2c7662611eb144d4e6189f9d2d4c12462535e65
raxcl <70327089+raxcl@users.noreply.github.com>
2022-02-25 11:49:56 +0800

    Update HashUtils.java

--- blog-api/src/main/java/cn/raxcl/util/HashUtils.java
+++ blog-api/src/main/java/cn/raxcl/util/HashUtils.java
@@ -14,6 +14,7 @@ public class HashUtils {
 
 	private static final BCryptPasswordEncoder B_CRYPT_PASSWORD_ENCODER = new BCryptPasswordEncoder();
 
+
 	public static long getMurmurHash32(String str) {
 		int i = MurmurHash3.hash32(str);
 		return i < 0 ? Integer.MAX_VALUE - (long) i : i;
@@ -21,6 +22,15 @@ public class HashUtils {
 
 	public static boolean matchBc(CharSequence rawPassword, String encodedPassword) {
 		return B_CRYPT_PASSWORD_ENCODER.matches(rawPassword, encodedPassword);
+	}
+
+	/**
+	 * 用于生成密码
+	 * @param rawPassword 明文密码
+	 * @return String
+	 */
+	public static String getBc(CharSequence rawPassword) {
+		return B_CRYPT_PASSWORD_ENCODER.encode(rawPassword);
 	}
 
 }
