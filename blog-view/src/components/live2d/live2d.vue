aa82397584422c15f039e57d63e13e3a818953cc
Raxcl <70327089+Raxcl@users.noreply.github.com>
2022-03-02 16:01:07 +0800

    Update live2d.vue

--- blog-view/src/components/live2d/live2d.vue
+++ blog-view/src/components/live2d/live2d.vue
@@ -1,12 +1,12 @@
 <template>
-    <live2d
-      :style="style"
-      :model="[1, 53]"
-      :direction="direction"
-      :size="size"
-      :tips="tips"
-      :homePage="homePage"
-    ></live2d>
+  <live2d
+    :style="style"
+    :model="[1, 53]"
+    :direction="direction"
+    :size="size"
+    :tips="tips"
+    :homePage="homePage"
+  ></live2d>
 </template>
 
 <script>
@@ -14,38 +14,71 @@
  * 项目中引用包时，将 import 内容替换
  * import live2d from 'vue-live2d'
  */
-import live2d from 'vue-live2d'
+import live2d from "vue-live2d";
 export default {
-  name: 'Live2d',
+  name: "Live2d",
   components: {
-    live2d
+    live2d,
   },
-  data () {
+  data() {
     return {
-      direction: 'right',
+      direction: "right",
       // style: '',
-      style: 'position: fixed; bottom: 0; right: 0; zIndex: 1',
+      style: "position: fixed; bottom: 0; right: 0; zIndex: 1",
       width: 500,
       height: 500,
       size: 350,
-      homePage:"https://github.com/Raxcl",
+      homePage: "https://github.com/Raxcl",
       tips: {
-        mouseover: [{
-          selector: '.vue-live2d',
-          texts: ['这样可以修改默认语句']
-        }]
-      }
-    }
+        mouseover: [
+          {
+            selector: ".vue-live2d-tool .fa-comment",
+            texts: ["猜猜我要说些什么？", "欣赏一些有意思的短句？"],
+          },
+          {
+            selector: ".vue-live2d-tool .fa-user-circle",
+            texts: ["想看看我的朋友们吗？"],
+          },
+          {
+            selector: ".vue-live2d-tool .fa-street-view",
+            texts: ["变装！"],
+          },
+          {
+            selector: ".vue-live2d-tool .fa-camera-retro",
+            texts: ["一起拍照留念？"],
+          },
+          {
+            selector: ".vue-live2d-tool .fa-info-circle",
+            texts: ["(＾Ｕ＾)ノ~ＹＯ"],
+          },
+          {
+            selector: ".vue-live2d-tool .fa-times",
+            texts: ["就要说再见了吗？"],
+          },
+        ],
+        copy: [
+          {
+            selector: "document",
+            texts: ["你都复制了些什么呀，转载要记得加上出处哦！"],
+          },
+        ],
+        visibilitychange: [
+          {
+            selector: "document",
+            texts: ["哇，你终于回来了～"],
+          },
+        ],
+      },
+    };
   },
-  created () {
+  created() {
     // tips 未使用，可自行在组件绑定此参数，注意此参数只会初始化一次
-    this.tips = this.customTips
-  }
-}
+    this.tips = this.tips;
+  },
+};
 </script>
 
 <style>
-
 body {
   margin: 20px;
   padding: 0;
