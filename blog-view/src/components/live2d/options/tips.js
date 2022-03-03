a7f5859d11913e0ac1cdfd6fa5c2e8fc4cd75244
Raxcl <70327089+Raxcl@users.noreply.github.com>
2022-03-03 21:34:04 +0800

    一言接口扩展

---
+++ blog-view/src/components/live2d/options/tips.js
@@ -0,0 +1,29 @@
+export default {
+  copy: [{
+    selector: 'document',
+    texts: ['你都复制了些什么呀，转载要记得加上出处哦！']
+  }],
+  visibilitychange: [{
+    selector: 'document',
+    texts: ['哇，你终于回来了～']
+  }],
+  mouseover: [{
+    selector: '.vue-live2d-tool .fa-comment',
+    texts: ['猜猜我要说些什么？', '欣赏一些有意思的短句？']
+  }, {
+    selector: '.vue-live2d-tool .fa-user-circle',
+    texts: ['想看看我的朋友们吗？']
+  }, {
+    selector: '.vue-live2d-tool .fa-street-view',
+    texts: ['变装！']
+  }, {
+    selector: '.vue-live2d-tool .fa-camera-retro',
+    texts: ['一起拍照留念？']
+  }, {
+    selector: '.vue-live2d-tool .fa-info-circle',
+    texts: ['(＾Ｕ＾)ノ~ＹＯ']
+  }, {
+    selector: '.vue-live2d-tool .fa-times',
+    texts: ['就要说再见了吗？']
+  }]
+}
