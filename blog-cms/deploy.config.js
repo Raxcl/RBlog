286517706fafcb15fd5f2472ee3012b1cec35026
Raxcl <70327089+Raxcl@users.noreply.github.com>
2022-03-12 17:22:44 +0800

    头像更换

---
+++ blog-cms/deploy.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  projectName: "blog-cms",
+  privateKey: "C:\\Users\\Administrator\\Desktop\\secrty",
+  passphrase: "",
+  cluster: [],
+  prod: {
+    name: "生产环境",
+    script: "yarn build",
+    host: "81.71.87.241",
+    port: 22,
+    username: "root",
+    password: "Aa2407414779.",
+    distPath: "dist",
+    webDir: "/mnt/raxcl/blog_admin",
+    isRemoveRemoteFile: true,
+    isRemoveLocalFile: true,
+  },
+};
